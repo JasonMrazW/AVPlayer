50fa7b7cdc7d642754201ed2ed535a0f902865c5
bo <bocai.wang@foxmail.com>
2021-11-30 00:17:41 +0800

    complete audio player

--- src/SDLAudioPlayer.cpp
+++ src/SDLAudioPlayer.cpp
@@ -4,6 +4,13 @@
 
 #include "SDLAudioPlayer.h"
 
+Uint8 * SDLAudioPlayer::audio_track;
+Uint32 SDLAudioPlayer::audio_length;
+Uint8 * SDLAudioPlayer::audio_pos;
+char * SDLAudioPlayer::pcm_buffer;
+
+const Uint32 SDLAudioPlayer::SDL_EVENT_BUFFER_END = 1;
+
 SDLAudioPlayer::SDLAudioPlayer() : running(false){
 
 }
@@ -12,6 +19,20 @@ SDLAudioPlayer::~SDLAudioPlayer() {
 
 }
 
+struct CallbackObject
+        {
+    void onCallback(Uint8 *stream, int len)
+    {
+        // ....
+        std::cout << "cout " << len << std::endl;
+    }
+
+    static void forwardCallback(void *userdata, Uint8 *stream, int len)
+    {
+        static_cast<CallbackObject*>(userdata)->onCallback(stream, len);
+    }
+        };
+
 int SDLAudioPlayer::onInit(char *audioPath) {
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         fprintf(stderr, "SDL_Init() failed: %s\n", SDL_GetError());
@@ -19,15 +40,18 @@ int SDLAudioPlayer::onInit(char *audioPath) {
     }
 
     //init audio info
+    CallbackObject callbackObject;
+
+
     audioSpec = SDL_AudioSpec();
-    audioSpec.samples = 44100;
-    audioSpec.freq = 1024;
+    audioSpec.samples = 1024;
+    audioSpec.freq = 44100;
     audioSpec.format = AUDIO_S16LSB;
     audioSpec.channels = 2;
     audioSpec.silence = 0;
     audioSpec.callback = fillDataCallBack;
 
-    if (SDL_OpenAudio(&audioSpec, NULL) < 0) {
+    if ((audioDeviceId = SDL_OpenAudioDevice(nullptr,0,&audioSpec,&audioSpec, SDL_AUDIO_ALLOW_ANY_CHANGE)) < 2) {
         fprintf(stderr, "SDL_Init() failed: %s\n", SDL_GetError());
         return APP_STOP;
     }
@@ -36,7 +60,27 @@ int SDLAudioPlayer::onInit(char *audioPath) {
 }
 
 void SDLAudioPlayer::onClean() {
+    SDL_Quit();
+}
 
+void SDLAudioPlayer::onEvent(SDL_Event *event) {
+    switch (event->type) {
+        case SDL_USEREVENT:
+            if(!audioStream.read(pcm_buffer, pcm_buffer_size)) {
+                //循环播放
+                audioStream.clear();
+                audioStream.seekg(0, std::ios_base::beg);
+                audioStream.read(pcm_buffer, pcm_buffer_size);
+            }
+            audio_track = (Uint8 *)pcm_buffer;
+            audio_length = pcm_buffer_size;
+            audio_pos = audio_track;
+            std::cout << "read pcm data start" << std::endl;
+            break;
+        case SDL_QUIT:
+            running = false;
+            break;
+    }
 }
 
 void SDLAudioPlayer::play(char *audioPath) {
@@ -44,8 +88,62 @@ void SDLAudioPlayer::play(char *audioPath) {
     if (state != APP_START) {
         return;
     }
+
+    SDL_Event sdlEvent;
+
+    //open file
+    audioStream.open(audioPath, std::ios::in|std::ios::binary);
+
+    //running = true;
+    pcm_buffer = new char[pcm_buffer_size];
+    running = true;
+
+    SDL_PauseAudioDevice(audioDeviceId, 0);
+    SDL_CreateThread(sdl_thread_custom_event, "custom_event", nullptr);
+
+    while(running) {
+        while(SDL_PollEvent(&sdlEvent)) {
+            onEvent(&sdlEvent);
+        }
+    }
+
+    free(pcm_buffer);
+    onClean();
 }
 
 void SDLAudioPlayer::fillDataCallBack(void *userdata, Uint8 * stream, int len) {
+    SDL_memset(stream, 0, len);
+    if (audio_length == 0) {
+        return;
+    }
 
+    len = len > audio_length ? audio_length : len;
+
+    //混合播放
+    //SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
+
+    //单独播放audio_pos
+    memcpy(stream, audio_pos, len);
+    audio_pos += len;
+    audio_length -= len;
+
+    if (audio_length <= 0) {
+        notifyGetAudioFrame();
+    }
+}
+
+void SDLAudioPlayer::notifyGetAudioFrame() {
+    SDL_Event event;
+    event.type = SDL_USEREVENT;
+    SDL_PushEvent(&event);
+}
+
+// 线程运行函数
+int SDLAudioPlayer::sdl_thread_custom_event(void *){
+    // 延时 5 秒
+    SDL_Delay(1000);
+    // 创建自定义事件并发送到消息队列中去
+    SDL_Event sdlEvent;
+    sdlEvent.type = SDL_USEREVENT;
+    SDL_PushEvent(&sdlEvent);
 }
--- src/SDLAudioPlayer.h
+++ src/SDLAudioPlayer.h
@@ -5,6 +5,10 @@
 #ifndef AVPLAYER_SDLAUDIOPLAYER_H
 #define AVPLAYER_SDLAUDIOPLAYER_H
 #include <SDL.h>
+#include <iostream>
+#include <fstream>
+#include <SDL_events.h>
+#define SDL_CUSTOM_EVENT  (SDL_USEREVENT + 1)
 
 class SDLAudioPlayer {
 public:
@@ -16,20 +20,35 @@ public:
         APP_START = 1
     };
 
+
+    static void fillDataCallBack(void *userdata, Uint8 *stream, int len);
+
     void play(char* audioPath);
 
 private:
     bool running = false;
 
-    Uint8 *buffer;
-    Uint32 length; // audio file length
+    static Uint8 *audio_track;
+    static Uint32 audio_length; // audio file length
+    static Uint8 *audio_pos; //current position
+    static const Uint32 SDL_EVENT_BUFFER_END;
     SDL_AudioSpec audioSpec;
+    SDL_AudioDeviceID audioDeviceId;
+
+    static char * pcm_buffer;
+    int pcm_buffer_size = 4096;
+    std::ifstream audioStream;
 
     int onInit(char *audioPath);
 
     void onClean();
 
-    static void fillDataCallBack(void *userdata, Uint8 *stream, int len);
+    void onEvent(SDL_Event *event);
+
+
+    static void notifyGetAudioFrame();
+
+    static int sdl_thread_custom_event(void *);
 };
 
 
