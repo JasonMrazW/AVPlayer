ed4f870c37d89b7ef566a7e8c2369ad3ad8973fa
bo <bocai.wang@foxmail.com>
2021-12-02 19:44:06 +0800

    add aac file

---
+++ src/parser/AACParser.h
@@ -0,0 +1,14 @@
+//
+// Created by bo on 2021/12/2.
+//
+
+#ifndef AVPLAYER_AACPARSER_H
+#define AVPLAYER_AACPARSER_H
+
+
+class AACParser {
+
+};
+
+
+#endif //AVPLAYER_AACPARSER_H
---
+++ src/parser/header/AACParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bo on 2021/12/2.
+//
+
+#include "../AACParser.h"
