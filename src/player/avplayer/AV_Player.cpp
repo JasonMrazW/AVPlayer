4fbd1fc2624e8daa5002a2a5bf552f8556afca6d
bo <bocai.wang@foxmail.com>
2021-12-23 23:58:01 +0800

    add some description

--- src/player/avplayer/AV_Player.cpp
+++ src/player/avplayer/AV_Player.cpp
@@ -7,7 +7,6 @@
 AV_Player::AV_Player() {
     render = new AV_Render_SDL();
     render->init();
-
     demuxer = new AVDemuxer();
 }
 
@@ -15,15 +14,20 @@ AV_Player::~AV_Player() {
 
 }
 
+//播放对应的地址
 void AV_Player::start(std::string url) {
     std::thread demuxer_thread = thread(std::ref(loadDemuxerCallback), std::ref(*demuxer), url);
 
+    //等待demux解码出第一帧
     while (demuxer->getYUVBuffer() == nullptr || demuxer->getPCMBuffer() == nullptr) {
         std::this_thread::sleep_for(std::chrono::microseconds(1));
     }
 
+    //将缓冲区的指针给Render，让Render知道去哪取数据
     render->setBuffer(demuxer->getYUVBuffer(), demuxer->getPCMBuffer());
     render->start();
+
+    //结束
     demuxer_thread.join();
 }
 
